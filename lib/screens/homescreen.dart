84e09fa2094f64b693204f013a6e49699c38dcfd
AlyElashram <71949028+AlyElashram@users.noreply.github.com>
2022-06-19 05:38:17 +0200

    -Added New filtered list to movie controller -Added Genres to set when fetching movies -Added Function to get filtered movies in Controller -Set Condition for List items of isFilter -Added Button to the UI to filter -Scrolling hides filter in MainScreen

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:movieapp/controllers/movieController.dart';
@@ -16,7 +17,10 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   MovieController controller = Get.put(MovieController());
+  bool isFilter = false;
+  String genre = 'Genre';
   int _selectedindex = 0;
+  bool hide = false;
   @override
   void initState() {
     super.initState();
@@ -24,6 +28,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    ScrollController _scrollController = ScrollController();
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
@@ -53,21 +58,65 @@ class _HomeScreenState extends State<HomeScreen> {
           children: [
             Expanded(
               flex: 1,
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Movies",
-                      style: TextStyle(
-                          fontFamily: 'Circular',
-                          color: Color(0xFF2C3F58),
-                          fontSize: 28),
+              child: hide
+                  ? Container(
+                      decoration: BoxDecoration(
+                          border: Border(
+                              bottom: BorderSide(
+                                  width: 1, color: Color(0xFFB1C4DC)))),
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Movies',
+                              style: TextStyle(
+                                  fontFamily: 'Circular',
+                                  fontSize: 14,
+                                  color: Color(0xFF2C3F58)),
+                            ),
+                            SizedBox(height: 14)
+                          ],
+                        ),
+                      ),
+                    )
+                  : Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Movies",
+                                style: TextStyle(
+                                    fontFamily: 'Circular',
+                                    color: Color(0xFF2C3F58),
+                                    fontSize: 28),
+                              ),
+                              Spacer(),
+                              ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(20)),
+                                      elevation: 0,
+                                      primary: Color(0xFFD7E1EA)),
+                                  onPressed: () {
+                                    _showActionSheet(context);
+                                  },
+                                  child: Text(genre,
+                                      style: TextStyle(
+                                          fontFamily: 'Circular',
+                                          fontSize: 12,
+                                          color: Color(0xFF4E5E71))))
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                  ],
-                ),
-              ),
             ),
             Expanded(
               flex: 5,
@@ -75,34 +124,107 @@ class _HomeScreenState extends State<HomeScreen> {
                 width: width - 20,
                 height: height - 20,
                 child: GetBuilder<MovieController>(builder: (_) {
-                  List<Movie> movies = _.movies;
-                  return ListView.builder(
-                      itemCount: movies.length,
-                      itemBuilder: (context, index) {
-                        return GestureDetector(
-                          onTap: () {
-                            Get.to(() => MovieDetails(movie: _.movies[index]));
-                          },
-                          child: Card(
-                              shape: Border(
-                                  bottom: BorderSide(
-                                width: 0.5,
-                                color: Color(0xFFB1C4DC),
-                              )),
-                              color: Color(0xFFFAFBFD),
-                              shadowColor: null,
-                              surfaceTintColor: null,
-                              borderOnForeground: false,
-                              elevation: 0,
-                              child:
-                                  movieListTile(width, height, movies[index])),
-                        );
-                      });
+                  return NotificationListener(
+                    onNotification: (notification) {
+                      if (notification == ScrollUpdateNotification) {}
+                      if (_scrollController.position.pixels > 105) {
+                        setState(() {
+                          hide = true;
+                        });
+                      } else {
+                        setState(() {
+                          hide = false;
+                        });
+                      }
+                      return true;
+                    },
+                    child: ListView.builder(
+                        controller: _scrollController,
+                        itemCount:
+                            isFilter ? _.filtered.length : _.movies.length,
+                        itemBuilder: (context, index) {
+                          return GestureDetector(
+                            onTap: () {
+                              Get.to(() => MovieDetails(
+                                  movie: isFilter
+                                      ? _.filtered[index]
+                                      : _.movies[index]));
+                            },
+                            child: Card(
+                                shape: Border(
+                                    bottom: BorderSide(
+                                  width: 0.5,
+                                  color: Color(0xFFB1C4DC),
+                                )),
+                                color: Color(0xFFFAFBFD),
+                                shadowColor: null,
+                                surfaceTintColor: null,
+                                borderOnForeground: false,
+                                elevation: 0,
+                                child: movieListTile(
+                                    width,
+                                    height,
+                                    isFilter
+                                        ? _.filtered[index]
+                                        : _.movies[index])),
+                          );
+                        }),
+                  );
                 }),
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  List<CupertinoActionSheetAction> _createActions() {
+    Set<String> genres = controller.genres;
+    List<CupertinoActionSheetAction> actions = [];
+    actions.add(CupertinoActionSheetAction(
+      isDefaultAction: true,
+      onPressed: () {
+        controller.getMoviesAndGenres();
+        setState(() {
+          genre = 'Genre';
+          isFilter = false;
+          Get.back();
+        });
+      },
+      child: Text(
+        'No filter',
+        style: TextStyle(
+            fontFamily: 'Circular', fontSize: 17, color: Color(0xFF2C3F58)),
+      ),
+    ));
+    genres.forEach((element) {
+      actions.add(CupertinoActionSheetAction(
+        onPressed: () {
+          controller.getFilteredMovies(element);
+          setState(() {
+            genre = element;
+            isFilter = true;
+            Get.back();
+          });
+        },
+        child: Text(element,
+            style: TextStyle(
+                fontFamily: 'Circular',
+                fontSize: 17,
+                color: Color(0xFF2C3F58))),
+      ));
+    });
+    return actions;
+  }
+
+  void _showActionSheet(BuildContext context) {
+    showCupertinoModalPopup<void>(
+      context: context,
+      builder: (BuildContext context) => CupertinoActionSheet(
+        title: const Text('Available Genres'),
+        message: const Text('Choose a genre to filter by'),
+        actions: _createActions(),
       ),
     );
   }
